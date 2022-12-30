e0c74fda5300ab031df3b809279b353eda7d4bc6
Jannick-dotcom <64270059+Jannick-dotcom@users.noreply.github.com>
2022-12-30 19:34:48 +0000

    fix connections from bias neuron

--- inc/layer.hpp
+++ inc/layer.hpp
@@ -18,40 +18,39 @@ public:
         this->neurons = new Neuron[ctNeurons+1];
         this->nextLayer = nullptr;
         this->prevLayer = prevLayer;
-        //TODO: change code layout for input layer bias node
-        if (prevLayer != nullptr)
+        
+        for (u_int16_t i = 0; i < ctNeurons; i++)
         {
-            for (u_int16_t i = 0; i < ctNeurons; i++)
+            neurons[i].activationFunction = ActivationFunction;
+            if (i == ctNeurons) // bias neuron
             {
-                neurons[i].connectionsIn = new connection[prevLayer->ctNeurons+1]; //connections from previous layer to this layer
-                neurons[i].ctConnectionsIn = prevLayer->ctNeurons;  //count of connections from previous layer to this layer
-                neurons[i].activationFunction = ActivationFunction;
-                if(i == ctNeurons) //bias neuron
-                {
-                    neurons[i].outputVal = 1; //bias neuron
-                    neurons[i].activationFunction = nullptr; //TODO: if activation function is nullptr, then neuron is bias neuron
-                    neurons[i].connectionsIn = nullptr;
-                    neurons[i].ctConnectionsIn = 0;
-                    continue; //skip to next neuron because bias neuron has no incoming connections
-                }
-
-                for (int neurPrevLayer = 0; neurPrevLayer < prevLayer->ctNeurons; neurPrevLayer++) //for each connection to currentNeuron
-                {
-                    neurons[i].connectionsIn[neurPrevLayer].inputVal = &prevLayer->neurons[neurPrevLayer].outputVal; //set start of connection to previous layer's neuron
-                    neurons[i].connectionsIn[neurPrevLayer].outputVal = &neurons[i].inputVal; //set end of connection to current layer's neuron
-                    neurons[i].connectionsIn[neurPrevLayer].weight = rand() / double(RAND_MAX) - 0.5; //set weight of connection to random value between -0.5 and 0.5
-                    neurons[i].connectionsIn[neurPrevLayer].fromNeuron = &prevLayer->neurons[neurPrevLayer];
-                    neurons[i].connectionsIn[neurPrevLayer].toNeuron = &neurons[i];
-                }
-            }
-        }
-        else
-        {
-            for (int i = 0; i < ctNeurons; i++)
-            {
-                neurons[i].activationFunction = ActivationFunction;
+                neurons[i].outputVal = 1;                // bias neuron
+                neurons[i].activationFunction = nullptr; // TODO: if activation function is nullptr, then neuron is bias neuron
                 neurons[i].connectionsIn = nullptr;
                 neurons[i].ctConnectionsIn = 0;
+                continue; // skip to next neuron because bias neuron has no incoming connections
+            }
+
+            if (prevLayer != nullptr)
+            {
+                neurons[i].connectionsIn = new connection[prevLayer->ctNeurons + 1]; // connections from previous layer to this layer
+                neurons[i].ctConnectionsIn = prevLayer->ctNeurons+1;                   // count of connections from previous layer to this layer
+            }
+            else
+            {
+                neurons[i].connectionsIn = nullptr;
+                neurons[i].ctConnectionsIn = 0;
+                continue; // skip to next neuron because input layer has no incoming connections
+            }
+
+
+            for (int neurPrevLayer = 0; neurPrevLayer < prevLayer->ctNeurons+1; neurPrevLayer++) // for each connection to currentNeuron
+            {
+                neurons[i].connectionsIn[neurPrevLayer].inputVal = &prevLayer->neurons[neurPrevLayer].outputVal; // set start of connection to previous layer's neuron
+                neurons[i].connectionsIn[neurPrevLayer].outputVal = &neurons[i].inputVal;                        // set end of connection to current layer's neuron
+                neurons[i].connectionsIn[neurPrevLayer].weight = rand() / double(RAND_MAX) - 0.5;                // set weight of connection to random value between -0.5 and 0.5
+                neurons[i].connectionsIn[neurPrevLayer].fromNeuron = &prevLayer->neurons[neurPrevLayer];
+                neurons[i].connectionsIn[neurPrevLayer].toNeuron = &neurons[i];
             }
         }
     }
