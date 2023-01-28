69397977f4b080a2809cdb295420181b30fc8302
Jannick-dotcom <64270059+Jannick-dotcom@users.noreply.github.com>
2023-01-28 15:47:57 +0000

    export activation function to enum

--- inc/activationFun.hpp
+++ inc/activationFun.hpp
@@ -3,74 +3,61 @@
 #include <string>
 #include <cmath>
 
-class ActivationFunction {
-public:
-    virtual double operator()(double x) = 0;
-    virtual double derivative(double x) = 0;
-    std::string name;
-};
+typedef enum {
+    LINEAR,
+    SIGMOID,
+    TANH,
+    RELU,
+    LEAKYRELU,
+    NONE
+} ActivationFunctionType;
 
-class Linear : public ActivationFunction {
-public:
-    double operator()(double x) override {
-        return x;
+double activationFunction(ActivationFunctionType type, double input)
+{
+    switch (type)
+    {
+    case LINEAR:
+        return input;
+        break;
+    case SIGMOID:
+        return 1.0 / (1.0 + exp(-input));
+        break;
+    case TANH:
+        return tanh(input);
+        break;
+    case RELU:
+        return input > 0 ? input : 0;
+        break;
+    case LEAKYRELU:
+        return input > 0 ? input : 0.01 * input;
+        break;
+    default:
+        return input;
+        break;
     }
-    double derivative(double x) override {
+}
+
+double activationFunctionDerivative(ActivationFunctionType type, double input)
+{
+    switch (type)
+    {
+    case LINEAR:
         return 1.0;
+        break;
+    case SIGMOID:
+        return activationFunction(type, input) * (1.0 - activationFunction(type, input));
+        break;
+    case TANH:
+        return 1.0 / pow(cosh(input),2);
+        break;
+    case RELU:
+        return input > 0 ? 1.0 : 0.0;
+        break;
+    case LEAKYRELU:
+        return input > 0 ? 1.0 : 0.01;
+        break;
+    default:
+        return 1.0;
+        break;
     }
-    Linear() {
-        name = "Linear";
-    }
-};
-
-class Sigmoid : public ActivationFunction {
-public:
-    double operator()(double x) override {
-        return 1.0 / (1.0 + exp(-x));
-    }
-    double derivative(double x) override {
-        return (*this)(x) * (1.0 - (*this)(x));
-    }
-    Sigmoid() {
-        name = "Sigmoid";
-    }
-};
-
-class Tanh : public ActivationFunction {
-public:
-    double operator()(double x) override {
-        return tanh(x);
-    }
-    double derivative(double x) override {
-        return 1.0 / pow(cosh(x),2);
-    }
-    Tanh() {
-        name = "Tanh";
-    }
-};
-
-class ReLU : public ActivationFunction {
-public:
-    double operator()(double x) override {
-        return x > 0.0 ? x : 0.0;
-    }
-    double derivative(double x) override {
-        return x > 0.0 ? 1.0 : 0.0;
-    }
-    ReLU() {
-        name = "ReLU";
-    }
-};
-
-class LeakyReLU : public ActivationFunction {
-public:
-    double operator()(double x) override {
-        return x > 0.0 ? x : 0.01 * x;
-    }
-    double derivative(double x) override {
-        return x > 0.0 ? 1.0 : 0.01;
-    }
-    LeakyReLU() {
-        name = "LeakyReLU";
-    }
-};
+}
