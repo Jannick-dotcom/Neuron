2fbf6c23bd4e48963c9e62763ec1f6ce6f29178a
Jannick <janni.cool@yahoo.de>
2023-05-23 18:07:34 +0000

    improve mutation helpers

--- inc/mutationHelper.hpp
+++ inc/mutationHelper.hpp
@@ -3,36 +3,49 @@
 #include "neuron.hpp"
 #include "connection.hpp"
 
+void copySingleNeuron(Neuron *source, Neuron *target)
+{
+    target->type = source->type;
+    target->inputVal = source->inputVal;
+    target->outputVal = source->outputVal;
+    target->gradientW = source->gradientW;
+    target->connectionsIn = source->connectionsIn;
+    target->ctConnectionsIn = source->ctConnectionsIn;
+    
+    if(source->ctConnectionsIn == 0) return;
+
+    for(uint16_t j = 0; j < source->ctConnectionsIn; j++)
+    {
+        target->connectionsIn[j].toNeuron = target;
+        target->connectionsIn[j].outputVal = &target->inputVal;
+
+        target->connectionsIn[j].fromNeuron = source->connectionsIn[j].fromNeuron;
+        target->connectionsIn[j].inputVal = source->connectionsIn[j].inputVal;
+    }
+}
+
 void copyNeuronExcept(Neuron *newNeurons, Neuron *old, uint16_t count, uint16_t index)
 {
     for(uint16_t i = 0; i < count+1; i++) //copy all neurons except the one to be removed
     {
+        Neuron *newatindex;
+        Neuron *oldatindex = &old[i];
         if(i < index)
         {
-            Neuron *newatindex = &newNeurons[i];
-            Neuron *oldatindex = &old[i];
-            newatindex->type = oldatindex->type;
-            newatindex->inputVal = oldatindex->inputVal;
-            newatindex->outputVal = oldatindex->outputVal;
-            newatindex->gradientW = oldatindex->gradientW;
-            newatindex->connectionsIn = oldatindex->connectionsIn;
-            newatindex->ctConnectionsIn = oldatindex->ctConnectionsIn;
+            newatindex = &newNeurons[i];
         }
         else if(i > index)
         {
-            Neuron *newatindex = &newNeurons[i-1];
-            Neuron *oldatindex = &old[i];
-            newatindex->type = oldatindex->type;
-            newatindex->inputVal = oldatindex->inputVal;
-            newatindex->outputVal = oldatindex->outputVal;
-            newatindex->gradientW = oldatindex->gradientW;
-            newatindex->connectionsIn = oldatindex->connectionsIn;
-            newatindex->ctConnectionsIn = oldatindex->ctConnectionsIn;
+            newatindex = &newNeurons[i-1];
         }
+        else
+            continue;
+            
+        copySingleNeuron(oldatindex, newatindex);
     }
 }
 
-uint8_t isBiasNeuron(const Neuron *Neuron)
+bool isBiasNeuron(const Neuron *Neuron)
 {
     if(Neuron->connectionsIn == nullptr) return 1;
     else return 0;
