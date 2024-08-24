3427f867787156f445a05e76bb4ebfcbdc609770
Jannick <jannick9743@gmail.com>
2024-08-24 23:20:13 +0200

    add memory alloc operator overrides

--- inc/neuronTypes.hpp
+++ inc/neuronTypes.hpp
@@ -1,6 +1,37 @@
 #pragma once
 #include <stdint.h>
 
+#ifdef useGPU
+void* operator new(size_t size)
+{
+    void *temp;
+    cudaError_t ret = cudaMallocManaged(&temp, size);
+    if(ret != cudaError::cudaSuccess)
+    {
+        printf("Layer: Malloc failed with code %d\n", ret);
+        exit(1);
+    }
+    return temp;
+}
+void operator delete(void* ptr)
+{
+    cudaError_t ret = cudaFree(ptr);
+    if(ret != cudaError::cudaSuccess)
+    {
+        printf("Layer: delete %p failed with code %d\n", ptr, ret);
+        exit(1);
+    }
+}
+void operator delete[](void* ptr)
+{
+    cudaError_t ret = cudaFree(ptr);
+    if(ret != cudaError::cudaSuccess)
+    {
+        printf("Layer: delete[] failed with code %d\n", ret);
+        exit(1);
+    }
+}
+#endif
 typedef float weight_t;
 typedef float in_out_t;
 typedef uint16_t count_t;
